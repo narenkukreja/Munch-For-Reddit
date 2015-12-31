10546b481729a97cc579ae0c5dbc398c9dac86b2
Naren Kukreja <narenkukreja@gmail.com>
2015-12-31 18:27:08 -0500

    Added new sceenshots

--- app/src/main/java/com/example/naren/munch/MainActivity.java
+++ app/src/main/java/com/example/naren/munch/MainActivity.java
@@ -36,8 +36,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
         mNavigationView = (NavigationView) findViewById(R.id.navigationView);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
